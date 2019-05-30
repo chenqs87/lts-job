f8f772083e680402dcf2707ff2c8018e616dca92
chenqingsong <chenqingsong@zhangyue.com>
2019-05-30 13:48:27 +0800

    format executor 导入数据逻辑调整

--- lts-job-executor-import/src/main/resources/CheckFileContent.sh
+++ lts-job-executor-import/src/main/resources/CheckFileContent.sh
@@ -1,9 +1,53 @@
 #!/usr/bin/env bash
 
+set -x
 execFile=$1
 params=$2
 
-echo "Path:::::${execFile}"
-echo "Params:::::${params}"
+echo "[CheckFileContent][INFO] script:${execFile}"
+echo "[CheckFileContent][INFO] script content: "
+echo "==========================================="
+cat ${execFile}
+echo "\n"
+echo "==========================================="
 
-cat /Users/chenqs/test/test.log | python ${execFile}
+echo "[CheckFileContent][INFO] input params: ${params}"
+
+inputFile="`echo ${params} | grep -Po 'input[":]+\K[^"]+'`"
+type="`echo ${params} | grep -Po 'type[":]+\K[^"]+'`"
+
+echo "[CheckFileContent][INFO] HDFS Input File: ${inputFile}"
+
+echo "[CheckFileContent][INFO] HDFS Input Type: ${type}"
+
+if [ -z "${inputFile}" ] || [ "x${inputFile}" = "x" ]; then
+    echo "[CheckFileContent][INFO] Param [input] is needed!"
+    exit -1
+fi
+
+if [ "x${type}" = "x" ]; then
+    echo "[CheckFileContent][INFO] Param [type] is needed!"
+    exit -1
+elif [ "x${type}" = "xold" ]; then
+    echo "[CheckFileContent][INFO] Will use config : ${OldVersionImportConfig}"
+else
+    echo "[CheckFileContent][INFO] Will use config : ${NewVersionImportConfig}"
+fi
+
+echo "[CheckFileContent][INFO] NewVersionImportConfig: ${NewVersionImportConfig}"
+
+echo "[CheckFileContent][INFO] OldVersionImportConfig: ${OldVersionImportConfig}"
+
+echo "[CheckFileContent][INFO] OldVersionImportConfig: ${OldVersionImportConfig}"
+
+
+exec_dir=$(cd `dirname $0`; pwd)
+
+# hdfs dfs get ${NewVersionImportConfig} ${exec_dir}
+
+config_file=$(echo "`cat ${NewVersionImportConfig}`")
+
+echo "[CheckFileContent][INFO] ConfigFilePath: ${config_file}"
+
+echo "------------------------- check input file ----------------------------"
+cat ${inputFile} | python ${execFile}
--- lts-job-executor/src/main/java/com/zy/data/lts/executor/config/ExecutorConfig.java
+++ lts-job-executor/src/main/java/com/zy/data/lts/executor/config/ExecutorConfig.java
@@ -1,40 +1,62 @@
 package com.zy.data.lts.executor.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
 /**
  * @author chenqingsong
  * @date 2019/4/9 17:56
  */
 @Configuration
+@ConfigurationProperties(prefix = "lts.server.executor")
 public class ExecutorConfig {
-    @Value("${lts.job.executor.dir:/tmp/lts-executor}")
-    private String executeRootDir;
 
-    public String getExecuteRootDir() {
-        return executeRootDir;
+    private static final String DEFAULT_EXECUTE_ROOT = "/tmp/lts-executor";
+    private String executeRoot;
+    private Map<String, String> executeEnv;
+
+    @PostConstruct
+    void init() {
+        if(StringUtils.isBlank(executeRoot)) {
+            executeRoot = DEFAULT_EXECUTE_ROOT;
+        }
+        System.out.println(executeEnv);
     }
 
-    public void setExecuteRootDir(String executeRootDir) {
-        this.executeRootDir = executeRootDir;
+    public void setExecuteRoot(String executeRootDir) {
+        this.executeRoot = executeRootDir;
     }
 
     public Path getExecDir(int flowTaskId, int taskId, int shard) throws IOException {
-        Path path = Paths.get(executeRootDir,
+        Path path = Paths.get(executeRoot,
                 String.valueOf(flowTaskId),
                 String.valueOf(taskId),
                 String.valueOf(shard));
 
         Files.createDirectories(path);
         return path;
+    }
+
+    public String getExecuteRoot() {
+        return executeRoot;
+    }
+
+    public Map<String, String> getExecuteEnv() {
+        return executeEnv;
+    }
+
+    public void setExecuteEnv(Map<String, String> executeEnv) {
+        this.executeEnv = executeEnv;
     }
 
     @Bean
