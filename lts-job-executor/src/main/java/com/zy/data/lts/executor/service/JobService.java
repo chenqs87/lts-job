75276a2a1be49ad018b2cd44fe2b303bcc6caaad
chenqingsong <chenqingsong@zhangyue.com>
2019-05-30 15:31:02 +0800

    bug 修复

--- lts-job-executor/src/main/java/com/zy/data/lts/executor/service/JobService.java
+++ lts-job-executor/src/main/java/com/zy/data/lts/executor/service/JobService.java
@@ -60,20 +60,24 @@ public class JobService implements ApplicationContextAware {
     ExecutorConfig executorConfig;
     private ApplicationContext applicationContext;
 
-    public void doExec(ExecuteRequest req) throws IOException {
+    private void doExec(ExecuteRequest req) {
+        try {
+            Task task = taskDao.findById(req.getFlowTaskId(), req.getTaskId());
+            Job job = jobDao.findById(task.getJobId());
 
+            FlowTask flowTask = flowTaskDao.findById(task.getFlowTaskId());
+            Path output = createOutputDir(req, job);
 
-        Task task = taskDao.findById(req.getFlowTaskId(), req.getTaskId());
-        Job job = jobDao.findById(task.getJobId());
+            String params = flowTask.getParams();
 
-        FlowTask flowTask = flowTaskDao.findById(task.getFlowTaskId());
-        Path output = createOutputDir(req, job);
+            JobExecuteEvent event = new JobExecuteEvent(task, output, params, job.getJobType());
 
-        String params = flowTask.getParams();
-
-        JobExecuteEvent event = new JobExecuteEvent(task, output, params, job.getJobType());
-
-        applicationContext.publishEvent(event);
+            applicationContext.publishEvent(event);
+        } catch (Exception e) {
+            logger.error("Fail to execute task. task info : {}", req, e);
+            JobResultRequest jrr = new JobResultRequest(req.getFlowTaskId(), req.getTaskId(), req.getShard());
+            adminApi.fail(jrr);
+        }
     }
 
     @Async(EXECUTOR_THREAD_POOL)
