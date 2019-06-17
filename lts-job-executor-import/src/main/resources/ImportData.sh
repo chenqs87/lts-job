8e5b365c546a65a6ef9255ab2c641db7aec9f441
chenqingsong <chenqingsong@zhangyue.com>
2019-06-17 14:38:35 +0800

    报警配置修改

--- lts-job-executor-import/src/main/resources/ImportData.sh
+++ lts-job-executor-import/src/main/resources/ImportData.sh
@@ -2,16 +2,11 @@
 #set -x
 params=$(echo "$1" | awk 'BEGIN{ORS=" "}{print $0}'| sed s/[[:space:]]//g)
 
-#导入数据老的程序包，对应项目rec-datatools,生产环境
-oldConfig="/user/pub/cqs/import-data/new/azkaban-job.zip"
-#老程序包灰度环境
-oldGreyConfig="/user/pub/cqs/import-data/new/azkaban-job.zip"
+oldConfig="/user/pub/ImportData/OldVersion/rec-datatools.zip"
+oldGreyConfig="/user/pub/ImportData/OldVersionGrey/rec-datatools.zip"
 
-#导入数据新程序包，对应项目data-x-zy,生产环境
-newConfig="/user/pub/cqs/import-data/new/azkaban-job.zip"
-
-#导入数据新程序包灰度环境
-newGreyConfig="/user/pub/cqs/import-data/new/azkaban-job.zip"
+newConfig="/user/pub/ImportData/NewVersion/azkaban-job.zip"
+newGreyConfig="/user/pub/ImportData/NewVersionGrey/azkaban-job.zip"
 
 hadoop_config="${HaoopConfigDir}"
 
@@ -20,6 +15,8 @@ echo "[ImportData][INFO] params:${params}"
 inputFile="`echo ${params} | grep -Po 'input[":]+\K[^"]+'`"
 
 type="`echo ${params} | grep -Po 'type[":]+\K[^"]+'`"
+version="`echo ${params} | grep -Po 'version[":]+\K[^"]+'`"
+
 
 execEnv="`echo ${params} | grep -Po 'env[":]+\K[^"]+'`"
 
@@ -32,7 +29,7 @@ java_zip_path=""
 if [ "x${type}" = "x" ]; then
     echo "[ImportData][INFO] Param [type] is needed!"
     exit -1
-elif [ "x${type}" = "xold" ]; then
+elif [ "x${version}" = "xold" ]; then
     if [ "x${execEnv}" = "xprod" ]; then
         java_zip_path="${oldConfig}"
     else
@@ -94,7 +91,7 @@ if [ ! -d "${hadoop_config}" ];then
     exit -1
 fi
 
-hadoop --config ${hadoop_config} jar ${jar_file} ${main_class} ${format_param}
+#hadoop --config ${hadoop_config} jar ${jar_file} ${main_class} ${format_param}
 
 exit $?
 
